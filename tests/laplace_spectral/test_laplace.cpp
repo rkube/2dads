8855e4e858095905a87683b2b817cc9622d8746f
rkube <ralph_kube@gmx.net>
2017-02-13 20:25:46 +0100

    verified test_derivs_spectral and laplace_spectral

--- tests/laplace_spectral/test_laplace.cpp
+++ tests/laplace_spectral/test_laplace.cpp
@@ -17,53 +17,66 @@
 
 using namespace std;
 using real_arr = cuda_array_bc_nogp<twodads::real_t, allocator_host>;
+using deriv_t = deriv_spectral_t<twodads::real_t, allocator_host>;
+using fft_t = fftw_object_t<twodads::real_t>;
 
 int main(void)
 {
     const size_t t_src{0};
     slab_config_js my_config(std::string("input_test_laplace_bs.json"));
+    deriv_t der{my_config.get_geom()};
+    fft_t myfft{my_config.get_geom(), my_config.get_dft_t()};
 
+    real_arr f(my_config.get_geom(), my_config.get_bvals(twodads::field_t::f_theta), 1);
+    real_arr sol_an(my_config.get_geom(), my_config.get_bvals(twodads::field_t::f_theta), 1);
+    real_arr sol_num(my_config.get_geom(), my_config.get_bvals(twodads::field_t::f_theta), 1);
+    
+    stringstream fname;
+
+    // Initialize input for laplace solver
+    f.apply([] (twodads::real_t dummy, size_t n, size_t m, twodads::slab_layout_t geom) -> twodads::real_t
     {
-        slab_bc my_slab(my_config);
-        real_arr sol_an(my_config.get_geom(), my_config.get_bvals(twodads::field_t::f_theta), 1);
-        real_arr sol_num(my_config.get_geom(), my_config.get_bvals(twodads::field_t::f_theta), 1);
-        
-        stringstream fname;
-        real_arr* arr_ptr{my_slab.get_array_ptr(twodads::field_t::f_omega)};
+        const twodads::real_t x{geom.get_x(n)};
+        const twodads::real_t y{geom.get_y(m)};
+        return(exp(-0.5 * (x * x + y * y)) * (-2.0 + x * x + y * y));
+    }, t_src);
+    myfft.dft_r2c(f.get_tlev_ptr(0), reinterpret_cast<twodads::cmplx_t*>(f.get_tlev_ptr(0)));
+    f.set_transformed(t_src, true);
+    sol_num.set_transformed(t_src, true);
+    der.invert_laplace(f, sol_num, t_src, t_src);
+    myfft.dft_c2r(reinterpret_cast<twodads::cmplx_t*>(sol_num.get_tlev_ptr(0)), sol_num.get_tlev_ptr(0));
+    utility :: normalize(sol_num, t_src);
+    sol_num.set_transformed(t_src, false);
 
-        // Initialize input for laplace solver
-        (*arr_ptr).apply([] (twodads::real_t dummy, size_t n, size_t m, twodads::slab_layout_t geom) -> twodads::real_t
-        {
-            const twodads::real_t x{geom.get_x(n)};
-            const twodads::real_t y{geom.get_y(m)};
-            return(exp(-0.5 * (x * x + y * y)) * (-2.0 + x * x + y * y));
-        }, t_src);
+    // Write numerical solution to file
+    fname.str(string(""));
+    fname << "test_laplace_solnum_" << my_config.get_nx() << "_host.dat";
+    utility :: print(sol_num, t_src, fname.str());
 
-        // Initialize analytic solution
-        sol_an.apply([] (twodads::real_t dummy, size_t n, size_t m, twodads::slab_layout_t geom) -> twodads::real_t
-                {
-                    const twodads::real_t x{geom.get_x(n)};
-                    const twodads::real_t y{geom.get_y(m)};
-                    return(exp(-0.5 * (x * x + y * y)));
-                },
-        t_src);
-        // The spectral solver returns an array without mean. 
-        // Find the mean of the analytic solution and subtract it.
-        sol_an -= utility :: mean(sol_an, t_src);
+    // Initialize analytic solution
+    sol_an.apply([] LAMBDACALLER (twodads::real_t dummy, size_t n, size_t m, twodads::slab_layout_t geom) -> twodads::real_t
+            {
+                const twodads::real_t x{geom.get_x(n)};
+                const twodads::real_t y{geom.get_y(m)};
+                return(exp(-0.5 * (x * x + y * y)));
+            },
+    t_src);
+    fname.str(string(""));
+    fname << "test_laplace_solan_" << my_config.get_nx() << "_host.dat";
+    utility :: print(sol_an, t_src, fname.str());
+    // The spectral solver returns an array without mean. 
+    // Find the mean of the analytic solution and subtract it.
+    const twodads::real_t mean{utility :: mean(sol_an, t_src)};
+    sol_an.elementwise([=] LAMBDACALLER (twodads::real_t lhs, twodads::real_t rhs) -> twodads::real_t
+    {
+        return(lhs - mean);
+    }, 0, 0);
 
-        fname.str(string(""));
-        fname << "test_laplace_solan_" << my_config.get_nx() << "_host.dat";
-        utility :: print(sol_an, t_src, fname.str());
+    //sol_an -= my_slab.get_array_ptr(twodads::field_t::f_strmf);
+    sol_num.elementwise([] LAMBDACALLER (twodads::real_t lhs, twodads::real_t rhs) -> twodads::real_t
+    {
+        return(lhs - rhs);
+    }, sol_an, 0, 0);
 
-        my_slab.invert_laplace(twodads::field_t::f_omega, twodads::field_t::f_strmf, t_src, t_src);
-
-        // Write numerical solution to file
-        fname.str(string(""));
-        fname << "test_laplace_solnum_" << my_config.get_nx() << "_host.dat";
-        utility :: print((*my_slab.get_array_ptr(twodads::field_t::f_strmf)), t_src, fname.str());
-
-        // Get the analytic solution
-        sol_an -= my_slab.get_array_ptr(twodads::field_t::f_strmf);
-        cout << "Nx = " << my_config.get_nx() << ", My = " << my_config.get_my() << ", L2 = " << utility :: L2(sol_an, t_src) << endl;
-    } // Let managed memory go out of scope before calling cudaDeviceReset()
+    cout << "Nx = " << my_config.get_nx() << ", My = " << my_config.get_my() << ", L2 = " << utility :: L2(sol_num, t_src) << endl;
 }
