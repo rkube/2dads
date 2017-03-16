7350b3d804d9a7c8b5467dd92a561ca77fb6a80c
rkube <ralph_kube@gmx.net>
2017-03-16 10:35:29 +0100

    rewrote test_laplace as to not use the slab interface

--- tests/laplace_fd/test_laplace.cpp
+++ tests/laplace_fd/test_laplace.cpp
@@ -1,5 +1,5 @@
 /*
- * Invert the laplace equation with boundary conditions in x using the cusparse tridiagonal solver with the new datatype
+ * Invert the laplace equation with boundary conditions in x using the cusparse tridiagonal solver 
  *
  * Invert
  * g(x,y) = exp(-(x^2 + y^2) / 2)
@@ -17,27 +17,27 @@
 
 using namespace std;
 using real_arr = cuda_array_bc_nogp<twodads::real_t, allocator_host>;
+using dft_t = fftw_object_t<twodads::real_t>;
+using deriv_t = deriv_fd_t<twodads::real_t, allocator_host>;
 
 int main(void)
 {
     const size_t t_src{0};
     slab_config_js my_config(std::string("input_test_laplace_fd.json"));
+    deriv_t der(my_config.get_geom());
+    dft_t dft(my_config.get_geom(), my_config.get_dft_t());
 
     {
-        slab_bc my_slab(my_config);
+        // Analytic solution
         real_arr sol_an(my_config.get_geom(), my_config.get_bvals(twodads::field_t::f_theta), 1);
+        // Input array
+        real_arr input(my_config.get_geom(), my_config.get_bvals(twodads::field_t::f_theta), 1);
+        // numerical solution
         real_arr sol_num(my_config.get_geom(), my_config.get_bvals(twodads::field_t::f_theta), 1);
-        
-        //my_slab.initialize_invlaplace(twodads::field_t::f_omega, tsrc);
-        //fname << "test_laplace_input_" << Nx << "_host.dat";
-        //utility :: print((*my_slab.get_array_ptr(twodads::field_t::f_omega)), tsrc, fname.str());
-        //cuda_array_bc_nogp<twodads::real_t, allocator_host> sol_an(my_geom, my_bvals, tlevs);
-
         stringstream fname;
-        real_arr* arr_ptr{my_slab.get_array_ptr(twodads::field_t::f_omega)};
 
         // Initialize input for laplace solver
-        (*arr_ptr).apply([] (twodads::real_t dummy, size_t n, size_t m, twodads::slab_layout_t geom) -> twodads::real_t
+        input.apply([] (twodads::real_t dummy, size_t n, size_t m, twodads::slab_layout_t geom) -> twodads::real_t
         {
             const twodads::real_t x{geom.get_x(n)};
             const twodads::real_t y{geom.get_y(m)};
@@ -57,18 +57,25 @@ int main(void)
         fname << "test_laplace_solan_" << my_config.get_nx() << "_host.dat";
         utility :: print(sol_an, t_src, fname.str());
 
-        my_slab.invert_laplace(twodads::field_t::f_omega, twodads::field_t::f_strmf, t_src, t_src);
+        dft.dft_r2c(input.get_tlev_ptr(t_src), reinterpret_cast<twodads::cmplx_t*>(input.get_tlev_ptr(t_src)));
+        input.set_transformed(t_src, true);
+
+        der.invert_laplace(input, sol_num, t_src, t_src);
+
+        dft.dft_c2r(reinterpret_cast<twodads::cmplx_t*>(sol_num.get_tlev_ptr(t_src)), sol_num.get_tlev_ptr(t_src));
+        utility :: normalize(sol_num, t_src);
+        sol_num.set_transformed(t_src, false);
 
         // Write numerical solution to file
         fname.str(string(""));
         fname << "test_laplace_solnum_" << my_config.get_nx() << "_host.dat";
-        utility :: print((*my_slab.get_array_ptr(twodads::field_t::f_strmf)), t_src, fname.str());
+        utility :: print(sol_num, t_src, fname.str());
 
         // Get the analytic solution
-        sol_an.elementwise([] LAMBDACALLER (twodads::real_t lhs, twodads::real_t rhs) -> twodads::real_t
+        sol_num.elementwise([] LAMBDACALLER (twodads::real_t lhs, twodads::real_t rhs) -> twodads::real_t
             {
                 return(lhs - rhs);
-            }, sol_an, 0, 0);
-        cout << "Nx = " << my_config.get_nx() << ", My = " << my_config.get_my() << ", L2 = " << utility :: L2(sol_an, t_src) << endl;
+            }, sol_an, t_src, t_src);
+        cout << "Nx = " << my_config.get_nx() << ", My = " << my_config.get_my() << ", L2 = " << utility :: L2(sol_num, t_src) << endl;
     } // Let managed memory go out of scope before calling cudaDeviceReset()
 }
